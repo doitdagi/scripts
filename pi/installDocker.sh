eb09fbaa1c79bb6d7e8cb28b66053007e7974397
doitdagi <doitdagi@gmail.com>
2016-09-02 09:09:23 +0000

    added install docker on pi script

---
+++ pi/installDocker.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+curl -sSL http://downloads.hypriot.com/docker-hypriot_1.8.1-1_armhf.deb >/tmp/docker-hypriot_1.8.1-1_armhf.deb
+
+sudo dpkg -i /tmp/docker-hypriot_1.8.1-1_armhf.deb
+
+rm -f /tmp/docker-hypriot_1.8.1-1_armhf.deb
+
+sudo sh -c 'usermod -aG docker $SUDO_USER'
+
+sudo systemctl enable docker.service
